6317315041adbd24f92c57d15d21b4dccbaf99cb
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-12-08 17:37:55 +0530

    introduce function isNaturalNumber()

--- src/factorialOfNumber.js
+++ src/factorialOfNumber.js
@@ -1,9 +1,13 @@
 const factorial = function(number){
-  if(number >= 1){
+  if(isNaturalNumber(number)){
     return number * factorial(number-1);
   }else{
     return 1;
   }
+}
+
+const isNaturalNumber = function(number){
+  return number >= 1;
 }
 
 module.exports = {factorial}
